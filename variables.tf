fa3f26da92e4d88e3fad886d04ef5cb37d27142b
Rajiv Kapoor <rajiv.kapoor@digital.hmrc.gov.uk>
2022-09-05 15:52:23 +0100

    Adding yet2 another random variable not violating any tfsec stuff

--- variables.tf
+++ variables.tf
@@ -2,3 +2,5 @@ variable "sandbox_account_id"         {}
 variable "free_account_id"         {}
 variable "fred_account_id"         {}
 variable "raj_account_id"         {}
+variable "rajiv_account_id"         {}
+variable "rajiv2_account_id"         {}
