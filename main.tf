a01014770a4a88d1cbaf81e84fe7f3625d75e99a
Rajiv Kapoor <rajiv.kapoor@digital.hmrc.gov.uk>
2022-09-09 13:42:14 +0100

    adding in main to do more testing

--- main.tf
+++ main.tf
@@ -10,37 +10,33 @@ provider "aws" {
   }
 }
 
-resource "aws_s3_bucket" "foo-bucket" {
-  region        = var.region
-  bucket        = local.bucket_name
-  force_destroy = true
+#resource "aws_s3_bucket" "foo-bucket" {
+#  region        = var.region
+#  bucket        = local.bucket_name
+#  force_destroy = true
+#
+#  tags = {
+#    Name = "foo-${data.aws_caller_identity.current.account_id}"
+#  }
+#  versioning {
+#    enabled = true
+#  }
+#  logging {
+#    target_bucket = "${aws_s3_bucket.log_bucket.id}"
+#    target_prefix = "log/"
+#  }
+#  server_side_encryption_configuration {
+#    rule {
+#      apply_server_side_encryption_by_default {
+#        kms_master_key_id = "${aws_kms_key.mykey.arn}"
+#        sse_algorithm     = "aws:kms"
+#      }
+#    }
+#  }
+#  acl           = "private"
+#}
 
-  tags = {
-    Name = "foo-${data.aws_caller_identity.current.account_id}"
-  }
-  versioning {
-    enabled = true
-  }
-  logging {
-    target_bucket = "${aws_s3_bucket.log_bucket.id}"
-    target_prefix = "log/"
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = "${aws_kms_key.mykey.arn}"
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-  acl           = "private"
-}
-
-#hello world commenting
-# more commenting to test this out
-# yet another comment to test out
-# even more stuff helloooooo there
-#yet one more
+#call a module
 module "raj_otherstuff" {
   source = "./modules/otherstuff"
 }
